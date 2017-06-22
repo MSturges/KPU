4e1e6b5e7862ba19916bf99003c567d5684d0601
MSturges <Sturges3232@gmail.com>
2017-06-22 16:47:08 -0600

    pushing code

--- server.js
+++ server.js
@@ -1,51 +1,39 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const compression = require('compression');
-const cors = require('cors');
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
 
-const app = express();
+var app = express()
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.all('*', (req, res, next) => {
-  res.sendFile('index.html', {
-    root: __dirname + '/dist/'
-  });
-});
+app.all('*', function(req,res,next) {
+  res.sendFile('index.html', { root: __dirname + '/public/' })
+})
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
     res.json({
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
   res.json({
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-module.exports = app;
+
+module.exports = app
